290a4c2b102516d1f0279bec1d913a5797ae5086
raphaelas <raphaelastrow@zoho.com>
2022-02-06 00:37:20 -0500

    Battery charged switch

--- toneAlarmClock/toneAlarmClock.ino
+++ toneAlarmClock/toneAlarmClock.ino
@@ -2,12 +2,10 @@
 #include "pitches.h"
 #include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(10, 11); // RX, TX
-
-const long ONE_MINUTE = 60000L;
 const int ONE_SECOND = 1000;
+const long ONE_MINUTE = long(ONE_SECOND) * 60;
 const long ONE_DAY = 86400000L;
-const long THREE_DAYS = 259200000L;
+const long THREE_DAYS = ONE_DAY * 3;
 
 const int COUNT_WEEKEND_DAYS = 2;
 const int WEEKEND_DAYS[COUNT_WEEKEND_DAYS] = {SATURDAY, SUNDAY};
@@ -17,14 +15,19 @@ const int DELAY_BETWEEN_REPEATS = 500, DELAY_BETWEEN_SWITCH_LISTENS = DELAY_BETW
 const int DELAY_DIVISOR = 10;
 
 const int STOP_ALARM_SWITCH = 2;
-const int UPDATE_TIME_SWITCH = 3;
-const int UPDATE_DAY_SWITCH = 4;
+const int BATTERY_CHARGED_SWITCH = 3;
+const int UPDATE_TIME_SWITCH = 4;
+const int UPDATE_DAY_SWITCH = 5;
+const int KEEP_BATTERY_ALIVE_LED = 6;
+const int TIME_OR_DAY_OR_BATTERY_CHARGED_IS_BEING_SET_LED = 7;
+const int PIEZO_PIN = 8;
+const int BATTERY_IS_LOW_LED = 9;
+const int RX_PIN = 10;
+const int TX_PIN = 11;
 
-const int KEEP_BATTERY_ALIVE_LED = 5;
-const int TIME_OR_DAY_IS_BEING_SET_LED = 6;
-const int BATTERY_IS_LOW_LED = 7;
+SoftwareSerial mySerial(RX_PIN, TX_PIN);
 
-const int KEEP_PORTABLE_BATTERY_ALIVE_COOLDOWN = 14000;
+const int KEEP_PORTABLE_BATTERY_ALIVE_COOLDOWN = 14500;
 const int BRIEF_MOMENT = 50, NOWISH = BRIEF_MOMENT;
 
 const long STARTER_WAKEUP_TIME = 180000L;
@@ -37,6 +40,7 @@ int startingDay;
 int serialDayIn;
 
 long timeLeftForBattery;
+unsigned long batteryChargedCheckpoint = 0;
 
 int selectedNotes[] = {
   NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
@@ -50,14 +54,17 @@ const float RECOMMENDED_NOTE_PAUSE_MULTIPLIER = 1.30;
 int stopAlarmSwitchState = 0;
 int updateTimeSwitchState = 0;
 int updateDaySwitchState = 0;
+int batteryChargedSwitchState = 0;
+
 bool keepSoundingAlarmClock = true;
 int MAX_COUNTDOWN = 4, countdownBlinkLightWhileAlarmSounding = MAX_COUNTDOWN;
 
 void setup() {
   pinMode(KEEP_BATTERY_ALIVE_LED, OUTPUT);
-  pinMode(TIME_OR_DAY_IS_BEING_SET_LED, OUTPUT);
+  pinMode(TIME_OR_DAY_OR_BATTERY_CHARGED_IS_BEING_SET_LED, OUTPUT);
   pinMode(BATTERY_IS_LOW_LED, OUTPUT);
   pinMode(STOP_ALARM_SWITCH, INPUT);
+  pinMode(BATTERY_CHARGED_SWITCH, INPUT);
   pinMode(UPDATE_TIME_SWITCH, INPUT);
   pinMode(UPDATE_DAY_SWITCH, INPUT);
   mySerial.begin(38400);
@@ -73,6 +80,7 @@ void loop() {
   keepPortableArduinoBatteryOn();
   listenToUpdateTimeSwitch();
   listenToUpdateDaySwitch();
+  checkBatteryChargedSwitchState();
   if (isTimeToSoundAlarm(currentMillisWithinDay, currentDayOfWeek)) {
     for (int note = 0; note < NOTE_COUNT; note++) {
       checkStopAlarmSwitchState();
@@ -90,10 +98,10 @@ void loop() {
 
 void playNote(int note) {
   int noteDuration = ONE_SECOND / noteDurations[note];
-  tone(8, selectedNotes[note], noteDuration);
+  tone(PIEZO_PIN, selectedNotes[note], noteDuration);
   int pauseBetweenNotes = noteDuration * RECOMMENDED_NOTE_PAUSE_MULTIPLIER;
   splitDelayToCheckForSwitchPress(pauseBetweenNotes);
-  noTone(8);
+  noTone(PIEZO_PIN);
 }
 
 void setWakeupTimeVariables(long theTimeUntilWakeup) {
@@ -129,6 +137,15 @@ void keepPortableArduinoBatteryOn() {
   }
 }
 
+int determineCorrectIndicatorLight() {
+  timeLeftForBattery = (batteryChargedCheckpoint + THREE_DAYS) - millis();
+  if (timeLeftForBattery > 0) {
+    return KEEP_BATTERY_ALIVE_LED;
+  } else {
+    return BATTERY_IS_LOW_LED;
+  }
+}
+
 void checkStopAlarmSwitchState() {
   stopAlarmSwitchState = digitalRead(STOP_ALARM_SWITCH);
   if (stopAlarmSwitchState == HIGH) {
@@ -138,12 +155,12 @@ void checkStopAlarmSwitchState() {
   }
 }
 
-int determineCorrectIndicatorLight() {
-  timeLeftForBattery = THREE_DAYS - millis();
-  if (timeLeftForBattery > 0) {
-    return KEEP_BATTERY_ALIVE_LED;
-  } else {
-    return BATTERY_IS_LOW_LED;
+void checkBatteryChargedSwitchState() {
+  batteryChargedSwitchState = digitalRead(BATTERY_CHARGED_SWITCH);
+  if (batteryChargedSwitchState == HIGH) {
+    batteryChargedCheckpoint = millis();
+    blinkLight(TIME_OR_DAY_OR_BATTERY_CHARGED_IS_BEING_SET_LED);
+    delay(DELAY_BETWEEN_SWITCH_LISTENS);
   }
 }
 
@@ -155,7 +172,7 @@ void listenToUpdateTimeSwitch() {
     long serialTimeIn = mySerial.parseInt();
     if (serialTimeIn > 0) {
       setWakeupTimeVariables(serialTimeIn);
-      blinkLight(TIME_OR_DAY_IS_BEING_SET_LED);
+      blinkLight(TIME_OR_DAY_OR_BATTERY_CHARGED_IS_BEING_SET_LED);
       keepSoundingAlarmClock = true;
     }
   }
@@ -169,7 +186,7 @@ void listenToUpdateDaySwitch() {
     serialDayIn = mySerial.parseInt();
     if (serialDayIn > 0) {
       startingDay = serialDayIn;
-      blinkLight(TIME_OR_DAY_IS_BEING_SET_LED);
+      blinkLight(TIME_OR_DAY_OR_BATTERY_CHARGED_IS_BEING_SET_LED);
       keepSoundingAlarmClock = true;
     }
   }
@@ -188,6 +205,7 @@ void splitDelayToCheckForSwitchPress(int delayAmount) {
   for (int i = 0; i < DELAY_DIVISOR; i++) {
     delay(delayAmount / DELAY_DIVISOR);
     checkStopAlarmSwitchState();
+    checkBatteryChargedSwitchState();
     listenToUpdateTimeSwitch();
     listenToUpdateDaySwitch();
   }
