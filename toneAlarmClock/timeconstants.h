be8eac3e10bdaa21517030775e0fc6eb793208ff
raphaelas <raphaelastrow@zoho.com>
2022-02-20 15:44:59 -0500

    Fix real issue: old python overcomplication

--- toneAlarmClock/timeconstants.h
+++ toneAlarmClock/timeconstants.h
@@ -1,9 +1,9 @@
 #ifndef timeconstants_h
 #define timeconstants_h
 
-const int ONE_SECOND = 1000;
-const int SECONDS_IN_MINUTE = 60; 
-const int MINUTES_IN_HOUR = SECONDS_IN_MINUTE;
+#define ONE_SECOND          1000
+#define SECONDS_IN_MINUTE   60
+#define MINUTES_IN_HOUR     SECONDS_IN_MINUTE
 const long ONE_MINUTE = long(ONE_SECOND) * SECONDS_IN_MINUTE;
 const long ONE_HOUR = ONE_MINUTE * MINUTES_IN_HOUR;
 const long ONE_DAY = ONE_HOUR * 24;
