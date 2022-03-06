7b5b76f77750cc37d9a1771ee82862c31b9d6657
raphaelas <raphaelastrow@zoho.com>
2022-03-06 18:54:18 -0500

    Uplink formatter

---
+++ FirstConfigurationBetter/uplinkFormatter.js
@@ -0,0 +1,13 @@
+function decodeUplink(input) {
+  var result = ""; 
+  for (var i = 0; i < input.bytes.length; i++) { 
+    result += String.fromCharCode(parseInt(input.bytes[i])); 
+  } 
+  return {
+    data: {
+      message: result
+    },
+    warnings: [],
+    errors: []
+  };
+}
