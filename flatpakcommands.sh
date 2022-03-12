c192e09842c273f8fa4a82981608cf0e99cf313b
raphaelas <raphaelastrow@zoho.com>
2022-03-12 15:12:19 -0500

    Flatpak with copy over x11

--- flatpakcommands.sh
+++ flatpakcommands.sh
@@ -1,4 +1,5 @@
 MYNAME=raphaelastrow
+rm -r dist
 flatpak-builder dist com.$MYNAME.timeuntilalarm.yml
 flatpak-builder --user --install --force-clean dist com.$MYNAME.timeuntilalarm.yml
 flatpak run com.$MYNAME.timeuntilalarm
--- timeuntilalarm.sh
+++ timeuntilalarm.sh
@@ -1,2 +1,3 @@
-python3 /app/bin/timeuntilalarm.py
+python3 /app/bin/timeuntilalarm.py | tail -n 1 | xsel --clipboard
+python3 /app/bin/timeuntilalarm.py | head -n 1
 sleep 5
